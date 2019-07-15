6bdadfcfe4c6b91b6fe945c640a0082fb1f49b14
Johannes Dobler <jdobler@protonmail.com>
2019-07-15 11:01:18 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# In case the script is executed from the script folder cd .. back
+# In case the script is executed from the script folder cd back to project root
 # this way the script can be called from script folder or from project root folder
 current=`pwd`
 current_dir=`basename "$current"`
@@ -8,13 +8,48 @@ if [ "$current_dir" == "scripts" ]; then
   cd ..
 fi
 
+##########
+## HELP ##
+##########
+if [[ ( "$1" == "-h" ) || ( "$1" == "--help" ) ]]; then
+    echo "Usage: `basename $0` [-h]"
+    echo "  Build the project"
+    echo
+    echo "  -h, --help           Show this help text"
+    echo "  -bt, --build_type    Can be one of [debug, release]"
+    exit 0
+fi
+
+###################################
+## VARIABLE SETTINGS && DEFAULTS ##
+###################################
 ROOTDIR=`pwd`
 CONFIG="debug"
 
-if [ "$1" == "debug" ] || [ "$1" == "release" ]; then
-  CONFIG="$1"
-fi
+#######################
+## PARAMETER PARSING ##
+#######################
+while :
+do
+    case "$1" in
+        -bt | --build_type)
+            if [ $# -ne 0 ]; then
+                CONFIG="$2"
+            fi
+            shift 2
+            ;;
+        "")
+            break
+            ;;
+        *)
+            echo -e "\033[33mWARNING: Argument $1 is unkown\033[0m"
+            shift 2 
+    esac
+done
 
+############
+## SCRIPT ##
+############
 BUILDFOLDER=$ROOTDIR/build/$CONFIG
 mkdir -p $BUILDFOLDER
 cd $BUILDFOLDER
